8bbb9da1aaf596e4a4a4054387c5fdd1e5009775
Cycodes <robert-cyby@hotmail.com>
2022-09-14 13:00:24 -0500

    configuracion de escritorio listo continuemos con la info de marcas

--- V12MIT/Escritorio.Designer.vb
+++ V12MIT/Escritorio.Designer.vb
@@ -20,20 +20,120 @@ Partial Class Escritorio
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Escritorio))
+        Me.MenuPrincipal = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuAdministarcionDropDown = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SucursalesMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsuariosMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DepartamentosMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MarcaMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TallasMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProductosMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuPrincipal.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuPrincipal
+        '
+        Me.MenuPrincipal.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.MenuAdministarcionDropDown})
+        Me.MenuPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.MenuPrincipal.Name = "MenuPrincipal"
+        Me.MenuPrincipal.Size = New System.Drawing.Size(854, 24)
+        Me.MenuPrincipal.TabIndex = 0
+        Me.MenuPrincipal.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CerrarToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(106, 22)
+        Me.CerrarToolStripMenuItem.Text = "Cerrar"
+        '
+        'MenuAdministarcionDropDown
+        '
+        Me.MenuAdministarcionDropDown.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SucursalesMenuItem, Me.UsuariosMenuItem, Me.DepartamentosMenuItem, Me.MarcaMenuItem, Me.TallasMenuItem, Me.ProductosMenuItem})
+        Me.MenuAdministarcionDropDown.Name = "MenuAdministarcionDropDown"
+        Me.MenuAdministarcionDropDown.Size = New System.Drawing.Size(100, 20)
+        Me.MenuAdministarcionDropDown.Text = "Administracion"
+        '
+        'SucursalesMenuItem
+        '
+        Me.SucursalesMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.SucursalesMenuItem.Image = Global.V12MIT.My.Resources.Resources.IconSucursal
+        Me.SucursalesMenuItem.Name = "SucursalesMenuItem"
+        Me.SucursalesMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.SucursalesMenuItem.Text = "Sucursales"
+        '
+        'UsuariosMenuItem
+        '
+        Me.UsuariosMenuItem.Image = Global.V12MIT.My.Resources.Resources.users_icon
+        Me.UsuariosMenuItem.Name = "UsuariosMenuItem"
+        Me.UsuariosMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.UsuariosMenuItem.Text = "Usuarios"
+        '
+        'DepartamentosMenuItem
+        '
+        Me.DepartamentosMenuItem.Image = Global.V12MIT.My.Resources.Resources.departamentos_icon
+        Me.DepartamentosMenuItem.Name = "DepartamentosMenuItem"
+        Me.DepartamentosMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.DepartamentosMenuItem.Text = "Departamentos"
+        '
+        'MarcaMenuItem
+        '
+        Me.MarcaMenuItem.Image = Global.V12MIT.My.Resources.Resources.marcas_icon
+        Me.MarcaMenuItem.Name = "MarcaMenuItem"
+        Me.MarcaMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.MarcaMenuItem.Text = "Marcas"
+        '
+        'TallasMenuItem
+        '
+        Me.TallasMenuItem.Image = Global.V12MIT.My.Resources.Resources.tallas_icon
+        Me.TallasMenuItem.Name = "TallasMenuItem"
+        Me.TallasMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.TallasMenuItem.Text = "Tallas"
+        '
+        'ProductosMenuItem
+        '
+        Me.ProductosMenuItem.Image = Global.V12MIT.My.Resources.Resources.productos_icon
+        Me.ProductosMenuItem.Name = "ProductosMenuItem"
+        Me.ProductosMenuItem.Size = New System.Drawing.Size(155, 22)
+        Me.ProductosMenuItem.Text = "Producots"
         '
         'Escritorio
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(30, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(854, 432)
+        Me.Controls.Add(Me.MenuPrincipal)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuPrincipal
         Me.Name = "Escritorio"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Escritorio"
+        Me.MenuPrincipal.ResumeLayout(False)
+        Me.MenuPrincipal.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents MenuPrincipal As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuAdministarcionDropDown As ToolStripMenuItem
+    Friend WithEvents SucursalesMenuItem As ToolStripMenuItem
+    Friend WithEvents UsuariosMenuItem As ToolStripMenuItem
+    Friend WithEvents DepartamentosMenuItem As ToolStripMenuItem
+    Friend WithEvents MarcaMenuItem As ToolStripMenuItem
+    Friend WithEvents TallasMenuItem As ToolStripMenuItem
+    Friend WithEvents ProductosMenuItem As ToolStripMenuItem
 End Class
--- V12MIT/Escritorio.vb
+++ V12MIT/Escritorio.vb
@@ -1,3 +1,42 @@
-﻿Public Class Escritorio
+﻿Imports System.ComponentModel
 
+Public Class Escritorio
+    Private Sub Escritorio_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Form1.Close()
+        Application.Exit()
+    End Sub
+    Private Sub Escritorio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MenuPrincipal.Renderer = New ToolStripProfessionalRenderer(New ColortablePersonalizada())
+
+
+        'cambiamos los colores de menus en menustrip
+
+        'menu archivo
+        ArchivoToolStripMenuItem.ForeColor = Color.White
+        CerrarToolStripMenuItem.ForeColor = Color.White
+
+        'menu administracion
+        MenuAdministarcionDropDown.ForeColor = Color.White
+        SucursalesMenuItem.ForeColor = Color.White
+        UsuariosMenuItem.ForeColor = Color.White
+        DepartamentosMenuItem.ForeColor = Color.White
+        MarcaMenuItem.ForeColor = Color.White
+        TallasMenuItem.ForeColor = Color.White
+        ProductosMenuItem.ForeColor = Color.White
+    End Sub
+    Private Sub SucursalesMenuItem_Click(sender As Object, e As EventArgs) Handles SucursalesMenuItem.Click
+        Sucursales.Show()
+    End Sub
+
+    Private Sub UsuariosMenuItem_Click(sender As Object, e As EventArgs) Handles UsuariosMenuItem.Click
+        UsuariosAdmin.Show()
+    End Sub
+
+    Private Sub DepartamentosMenuItem_Click(sender As Object, e As EventArgs) Handles DepartamentosMenuItem.Click
+        Departamentos.Show()
+    End Sub
+
+    Private Sub MarcaMenuItem_Click(sender As Object, e As EventArgs) Handles MarcaMenuItem.Click
+        Marcas.Show()
+    End Sub
 End Class
